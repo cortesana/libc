f0b2ad26680ca56fae7c57fd931c3726cf2e65e1
cortesana <a.chdez2020@gmail.com>
2020-05-08 02:35:51 +0200

    Update ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,5 +1,3 @@
-// string comparison of s1 and s2. If 0 is returned, s1 and s2 are the same.
-
 int	ft_strcmp(char *s1, char *s2)
 {
 	int i;
@@ -15,13 +13,5 @@ int	ft_strcmp(char *s1, char *s2)
 		i++;
 	}
 	printf("%d", 0);
-	return(0);
-}
-
-int main()
-{
-	char s1[] = "holA";
-	char s2[] = "hola";
-	ft_strcmp(s1, s2);
 	return(0);
 }
