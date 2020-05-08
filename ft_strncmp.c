07ce04c12f8d1f6ba27ba592947c163ac83c2136
cortesana <a.chdez2020@gmail.com>
2020-05-08 02:44:17 +0200

    Update ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,5 +1,3 @@
-// size-bounded string comparison.
-
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
@@ -14,15 +12,5 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		}
 		i++;
 	}
-	printf("%d", 0);
 	return(0);
-}
-
-int main()
-{
-	char s1[]= "holA";
-	char s2[]= "hola";
-	unsigned int n;
-	n = 4;
-	ft_strncmp(s1, s2, n);
 }
