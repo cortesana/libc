333e508764ec7905363f3d3d913e3645389596c2
cortesana <a.chdez2020@gmail.com>
2020-05-08 02:34:17 +0200

    Update ft_putchar

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,16 +1,5 @@
-// displays char c (converted to an 'unsigned char') with write().
-
-#include <unistd.h>
-
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 	write(1, "\n", 1);
-}
-int	main()
-{
-	char c;
-	
-	c = '0';
-	ft_putchar(c);
 }
