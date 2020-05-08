f715f0e82e93160a5c3dc39c0a0467df2ee96bbb
cortesana <a.chdez2020@gmail.com>
2020-05-08 02:44:42 +0200

    Update ft_strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,7 +1,3 @@
-// size-bounded copy of the string src to dst including the null-terminating character.
-
-#include <unistd.h>
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
@@ -16,13 +12,3 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	dest[i] = '\0';
 	return(dest);
 }
-
-int main()
-{
-	unsigned int n;
-	char dest[] = "guayaba";
-	char src[] = "hola";
-
-	n = 3;	
-	ft_strncpy(dest, src, n);
-} 			
